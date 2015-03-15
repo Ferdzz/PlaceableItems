4aebc75d4527acf263a2a41f50eef6f76b442e0c
Emmanuel Maximus Yohanes <geofanny.emmanuel@yahoo.co.id>
2015-03-15 14:30:25 +0700

    Tweaked disk bounds, removed unused imports

--- src/main/java/com/stuntmania/placeableitems/block/BlockDisk.java
+++ src/main/java/com/stuntmania/placeableitems/block/BlockDisk.java
@@ -2,16 +2,7 @@ package com.stuntmania.placeableitems.block;
 
 import java.util.Random;
 
-import com.stuntmania.placeableitems.PlaceableItems;
-import com.stuntmania.placeableitems.tileentity.TEDisk;
-import com.stuntmania.placeableitems.tileentity.TESnowball;
-
-import cpw.mods.fml.common.registry.GameRegistry;
-import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.item.EntityItem;
-import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -19,12 +10,16 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
 
+import com.stuntmania.placeableitems.tileentity.TEDisk;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+
 public class BlockDisk extends BlockPlaceableItems {
 	
 	public BlockDisk() {
 		super(Material.sponge);
 	//	setBlockTextureName(PlaceableItems.MODID + ":disk");
-		setBlockBounds(0.25F, 0, 0.25F, 0.75F, 0.5F, 0.75F);
+		setBlockBounds(0.225F, 0, 0.225F, 0.775F, 0.0625F, 0.775F);
 		setBlockName("diskBlock");
 		GameRegistry.registerBlock(this, "diskBlock");
 	}
