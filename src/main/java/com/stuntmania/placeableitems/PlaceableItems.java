e2b113ca48b732b956ca204f56920c519cc8e296
Frederic Deschenes <frederic.deschenes1@gmail.com>
2015-05-10 16:05:06 -0400

    1.4.2

--- src/main/java/com/stuntmania/placeableitems/PlaceableItems.java
+++ src/main/java/com/stuntmania/placeableitems/PlaceableItems.java
@@ -28,7 +28,7 @@ public class PlaceableItems {
 
 	public static final String NAME = "Placeable Items Mod";
 	public static final String MODID = "placeableitems";
-	public static final String VERSION = "1.4";
+	public static final String VERSION = "1.4.2";
 
 	@EventHandler
 	public void init(FMLInitializationEvent event) {
