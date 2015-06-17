c52be14b4e47f845c3eaf53736133b88d78d7dd6
Emmanuel Maximus Yohanes <geofanny.emmanuel@yahoo.co.id>
2015-06-17 17:19:11 +0700

    silly mistake, fixed.

--- src/main/java/com/stuntmania/placeableitems/tileentity/TEPumpkinPie.java
+++ src/main/java/com/stuntmania/placeableitems/tileentity/TEPumpkinPie.java
@@ -3,7 +3,7 @@ package com.stuntmania.placeableitems.tileentity;
 
 public class TEPumpkinPie extends TEPlaceableFood {
 
-	public TEPumpkinPie(int foodLevel, float saturation)
+	public TEPumpkinPie()
 	{
 		super(8, 0.3F);
 	}
