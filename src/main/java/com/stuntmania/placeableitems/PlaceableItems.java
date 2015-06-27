1041d847b5bedb59291c4f2638af129933835b98
Emmanuel Maximus Yohanes <geofanny.emmanuel@yahoo.co.id>
2015-06-27 19:00:52 +0700

    Implemented logger and exception handling for update checker

--- src/main/java/com/stuntmania/placeableitems/PlaceableItems.java
+++ src/main/java/com/stuntmania/placeableitems/PlaceableItems.java
@@ -1,5 +1,7 @@
 package com.stuntmania.placeableitems;
 
+import org.apache.logging.log4j.Logger;
+
 import com.stuntmania.placeableitems.handler.BlockBreakHandler;
 import com.stuntmania.placeableitems.handler.BlockDropsHandler;
 import com.stuntmania.placeableitems.handler.BucketPlaceHandler;
@@ -17,6 +19,7 @@ import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.common.MinecraftForge;
 
 @Mod(modid = PlaceableItems.MODID, name = PlaceableItems.NAME, version = PlaceableItems.VERSION)
@@ -30,14 +33,21 @@ public class PlaceableItems {
 	
 	public static UpdateChecker updateChecker;
 	public static boolean warned = false;
+	
+	public static Logger logger;
 
 	public static final String NAME = "Placeable Items Mod";
 	public static final String MODID = "placeableitems";
 	public static final String VERSION = "1.4.3";
 
 	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		logger = event.getModLog();
+	}
+	
+	@EventHandler
 	public void init(FMLInitializationEvent event) {
-		System.out.println("Started loading " + MODID + " version " + VERSION);
+		logger.info("Started loading " + MODID + " version " + VERSION);
 
 		ModBlocks.init();
 		ModItems.init();
@@ -54,6 +64,6 @@ public class PlaceableItems {
 	@EventHandler
 	public void postInit(FMLPostInitializationEvent event) {
 		proxy.checkUpdate();
-		System.out.println("Loaded " + MODID + " version " + VERSION + " correctly");
+		logger.info("Loaded " + MODID + " version " + VERSION + " correctly");
 	}
 }
--- src/main/java/com/stuntmania/placeableitems/utils/UpdateChecker.java
+++ src/main/java/com/stuntmania/placeableitems/utils/UpdateChecker.java
@@ -6,38 +6,45 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.logging.log4j.Logger;
 
 import com.stuntmania.placeableitems.PlaceableItems;
 
 public class UpdateChecker implements Runnable {
 	
+	Logger logger = PlaceableItems.logger;
+	
 	private static boolean upToDate = false;
 	private static String latest = "";
 
 	public void run() {
+		logger.info("Checking for mod updates...");
 		InputStream in = null;
 		try {
 			in = new URL("https://raw.githubusercontent.com/Ferdzz/PlaceableItems/7c2462633060e3466beead861a79bc389915ec7c/version.txt").openStream();
 		} catch (MalformedURLException e) {
+			logger.error("Unable to check for updates!");
 			e.printStackTrace();
 		} catch (IOException e) {
+			logger.error("Unable to check for updates!");
 			e.printStackTrace();
 		}
 		
 		try {
 			latest = IOUtils.readLines(in).get(0);
 		} catch (IOException e) {
+			logger.error("Unable to determine update!");
 			e.printStackTrace();
 		} finally {
 			IOUtils.closeQuietly(in);
 		}
 
-        System.out.println("Latest version = " + latest);
+        logger.info("Latest version is " + latest + ".");
         upToDate = PlaceableItems.VERSION.equals(latest);
         if (upToDate) {
-            System.out.println("Mod is the latest version!");
+            logger.info("Mod is the latest version!");
         } else {
-        	System.out.println("Mod is out of date! Please update.");
+        	logger.warn("Mod is out of date! Please update.");
         }
 	}
 
