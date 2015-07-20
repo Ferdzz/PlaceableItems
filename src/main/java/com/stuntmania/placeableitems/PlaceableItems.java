448a8200f43a6b45601cff289a427c4856f545b2
Emmanuel Maximus Yohanes <geofanny.emmanuel@yahoo.co.id>
2015-07-20 12:53:35 +0700

    Version

--- src/main/java/com/stuntmania/placeableitems/PlaceableItems.java
+++ src/main/java/com/stuntmania/placeableitems/PlaceableItems.java
@@ -38,7 +38,7 @@ public class PlaceableItems {
 
 	public static final String NAME = "Placeable Items Mod";
 	public static final String MODID = "placeableitems";
-	public static final String VERSION = "1.4.3";
+	public static final String VERSION = "1.4.4";
 
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
