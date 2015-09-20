c53df720388b44874a767f5aa44a6d818f096688
Frederic Deschenes <frederic.deschenes1@gmail.com>
2015-09-20 01:36:09 -0400

    Updated pumpkin pie model

--- src/main/java/com/stuntmania/placeableitems/tileentity/renderers/TESRPumpkinPie.java
+++ src/main/java/com/stuntmania/placeableitems/tileentity/renderers/TESRPumpkinPie.java
@@ -15,7 +15,7 @@ import com.stuntmania.placeableitems.tileentity.TEPumpkinPie;
 public class TESRPumpkinPie extends TileEntitySpecialRenderer {
 	
 	IModelCustom model = AdvancedModelLoader.loadModel(new ResourceLocation(PlaceableItems.MODID, "obj/pumpkin_pie.obj"));
-	ResourceLocation texture = new ResourceLocation("minecraft", "textures/items/pumpkin_pie.png");
+	ResourceLocation texture = new ResourceLocation(PlaceableItems.MODID, "textures/blocks/pumpkin_pie.png");
 	
 	@Override
 	public void renderTileEntityAt(TileEntity entity, double x, double y, double z, float p_147500_8_) {
@@ -26,8 +26,8 @@ public class TESRPumpkinPie extends TileEntitySpecialRenderer {
 		GL11.glPushMatrix();
 		GL11.glEnable(GL12.GL_RESCALE_NORMAL);
 		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-		GL11.glTranslatef((float) x + 0.5F, (float) y + 0.2F, (float) z + 0.5F);
-		GL11.glScalef(0.06F, 0.06F, 0.06F);
+		GL11.glTranslatef((float) x + 0.5F, (float) y + 0.0F, (float) z + 0.5F);
+		GL11.glScalef(1, 1, 1);
 		
 		int facing = facedEntity.getFacing();
 		int k = 0;
