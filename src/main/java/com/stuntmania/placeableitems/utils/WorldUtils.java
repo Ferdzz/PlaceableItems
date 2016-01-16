cd5e600a87d0b6b221211df6f9cd5dc35ea96649
Emmanuel Maximus Yohanes <geofanny.emmanuel@yahoo.co.id>
2016-01-16 19:14:35 +0700

    Added one more check for block placement
    Prevents placing blocks in entity's hitbox

--- src/main/java/com/stuntmania/placeableitems/utils/WorldUtils.java
+++ src/main/java/com/stuntmania/placeableitems/utils/WorldUtils.java
@@ -1,6 +1,7 @@
 package com.stuntmania.placeableitems.utils;
 
 import net.minecraft.block.Block;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
@@ -25,11 +26,13 @@ public class WorldUtils {
 	public static boolean placeBlockWithMetadata(int x, int y, int z, int face, Block block, int metadata, World world, EntityPlayer player) {
 		ForgeDirection direction = ForgeDirection.getOrientation(face);
 		if (world.getBlock(x + direction.offsetX, y + direction.offsetY, z + direction.offsetZ) == Blocks.air)
-			if (player.canPlayerEdit(x + direction.offsetX, y + direction.offsetY, z + direction.offsetZ, face, player.getCurrentEquippedItem())) {
-				world.setBlock(x + direction.offsetX, y + direction.offsetY, z + direction.offsetZ, block);
-				world.setBlockMetadataWithNotify(x + direction.offsetX, y + direction.offsetY, z + direction.offsetZ, metadata, 2);
-				block.onBlockPlacedBy(world, x + direction.offsetX, y + direction.offsetY, z + direction.offsetZ, player, player.getCurrentEquippedItem());
-				return true;
+			if (world.canPlaceEntityOnSide(block, x + direction.offsetX, y + direction.offsetY, z + direction.offsetZ, false, face, (Entity)null, player.getCurrentEquippedItem())) {
+				if (player.canPlayerEdit(x + direction.offsetX, y + direction.offsetY, z + direction.offsetZ, face, player.getCurrentEquippedItem())) {
+					world.setBlock(x + direction.offsetX, y + direction.offsetY, z + direction.offsetZ, block);
+					world.setBlockMetadataWithNotify(x + direction.offsetX, y + direction.offsetY, z + direction.offsetZ, metadata, 2);
+					block.onBlockPlacedBy(world, x + direction.offsetX, y + direction.offsetY, z + direction.offsetZ, player, player.getCurrentEquippedItem());
+					return true;
+				}
 			}
 		return false;
 	}
@@ -37,10 +40,12 @@ public class WorldUtils {
 	public static boolean placeBlockWithoutMetadata(int x, int y, int z, int face, Block block, World world, EntityPlayer player) {
 		ForgeDirection direction = ForgeDirection.getOrientation(face);
 		if (world.getBlock(x + direction.offsetX, y + direction.offsetY, z + direction.offsetZ) == Blocks.air)
-			if (player.canPlayerEdit(x + direction.offsetX, y + direction.offsetY, z + direction.offsetZ, face, player.getCurrentEquippedItem())) {
-				world.setBlock(x + direction.offsetX, y + direction.offsetY, z + direction.offsetZ, block);
-				block.onBlockPlacedBy(world, x + direction.offsetX, y + direction.offsetY, z + direction.offsetZ, player, player.getCurrentEquippedItem());
-				return true;
+			if (world.canPlaceEntityOnSide(block, x + direction.offsetX, y + direction.offsetY, z + direction.offsetZ, false, face, (Entity)null, player.getCurrentEquippedItem())) {
+				if (player.canPlayerEdit(x + direction.offsetX, y + direction.offsetY, z + direction.offsetZ, face, player.getCurrentEquippedItem())) {
+					world.setBlock(x + direction.offsetX, y + direction.offsetY, z + direction.offsetZ, block);
+					block.onBlockPlacedBy(world, x + direction.offsetX, y + direction.offsetY, z + direction.offsetZ, player, player.getCurrentEquippedItem());
+					return true;
+				}
 			}
 		return false;
 	}
