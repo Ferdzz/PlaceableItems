b28fab49e4e41c5cfd81c74d0564462ed7d9b819
Ferdzz <frederic.deschenes1@gmail.com>
2016-06-29 01:21:10 -0400

    Added lines texture when written book is placed

--- src/main/java/me/ferdz/placeableitems/block/BlockBookAndQuill.java
+++ src/main/java/me/ferdz/placeableitems/block/BlockBookAndQuill.java
@@ -2,22 +2,32 @@ package me.ferdz.placeableitems.block;
 
 import java.util.Random;
 
+import me.ferdz.placeableitems.block.state.EnumArrowType;
+import me.ferdz.placeableitems.block.state.EnumBook;
 import me.ferdz.placeableitems.init.ModBlocks;
 import me.ferdz.placeableitems.tileentity.TEBook;
 import net.minecraft.block.ITileEntityProvider;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyEnum;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemWritableBook;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 public class BlockBookAndQuill extends BlockPlaceableItems implements ITileEntityProvider {
 
+	public static final PropertyEnum<EnumBook> CONTENT = PropertyEnum.create("content", EnumBook.class);
+	
 	public BlockBookAndQuill(String name) {
 		super(name);
 	}
@@ -28,19 +38,39 @@ public class BlockBookAndQuill extends BlockPlaceableItems implements ITileEntit
 		if (te instanceof TEBook) {
 			((TEBook) te).setBook(stack);
 			worldIn.notifyBlockOfStateChange(pos, ModBlocks.blockBookAndQuill);
-			te.markDirty();
 		}
 	}
 	
 	@Override
 	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ) {
 		TileEntity te = worldIn.getTileEntity(pos);
-		if(te instanceof TEBook) { 
+		if(te != null && te instanceof TEBook) { 
 			playerIn.openBook(((TEBook) te).getBook(), hand);
 			return true;
 		}
 		return false;
 	}
+	
+	@Override
+	public IBlockState getActualState(IBlockState state, IBlockAccess worldIn, BlockPos pos) {
+		TileEntity te = worldIn.getTileEntity(pos);
+		if(te != null && te instanceof TEBook) {
+			ItemStack book = ((TEBook)te).getBook();
+			if(book.getTagCompound() != null) {
+				NBTTagList l = book.getTagCompound().getTagList("pages", 8);
+				for (int i = 0; i < l.tagCount(); i++) {
+					if(l.get(i).toString().equals("\"\""))
+						return state.withProperty(CONTENT, EnumBook.EMPTY);						
+				}
+				return state.withProperty(CONTENT, EnumBook.WRITTEN);}
+		}
+		return state;
+	}
+	
+	@Override
+	protected BlockStateContainer createBlockState() {
+        return new BlockStateContainer(this, new IProperty[]{ CONTENT, FACING });
+    }
 	
 	// handled in BlockBreakHandler
 	@Override
---
+++ src/main/java/me/ferdz/placeableitems/block/state/EnumBook.java
@@ -0,0 +1,30 @@
+package me.ferdz.placeableitems.block.state;
+
+import net.minecraft.util.IStringSerializable;
+
+public enum EnumBook implements IStringSerializable {
+    EMPTY(0, "empty"),
+    WRITTEN(1, "written");
+	
+    private int ID;
+    private String name;
+    
+    private EnumBook(int ID, String name) {
+        this.ID = ID;
+        this.name = name;
+    }
+    
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public int getID() {
+        return ID;
+    }
+    
+    @Override
+    public String toString() {
+        return getName();
+    }
+}
