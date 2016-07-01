37edb2baf8d076e28cc13c02b6fc846ea20c97e3
Ferdzz <frederic.deschenes1@gmail.com>
2016-06-30 21:36:21 -0400

    Missing files of last commit

--- src/main/java/me/ferdz/placeableitems/block/tool/BlockSword.java
+++ src/main/java/me/ferdz/placeableitems/block/tool/BlockSword.java
@@ -1,8 +1,78 @@
 package me.ferdz.placeableitems.block.tool;
 
+import me.ferdz.placeableitems.init.ModBlocks;
+import me.ferdz.placeableitems.state.tool.EnumSword;
+import me.ferdz.placeableitems.tileentity.tool.TESword;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyEnum;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+
 public class BlockSword extends BlockTool {
 
+	public static final PropertyEnum<EnumSword> MODEL = PropertyEnum.create("model", EnumSword.class);
+	
 	public BlockSword(String name) {
 		super(name);
+	}
+	
+	@Override
+	public IBlockState onBlockPlaced(World world, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
+		IBlockState state = super.onBlockPlaced(world, pos, facing, hitX, hitY, hitZ, meta, placer);
+		switch(facing) {
+		case UP:
+			return state.withProperty(MODEL, EnumSword.TOP);
+		default:
+			return state.withProperty(MODEL, EnumSword.SIDE0);
+		}
+	}
+	
+	@Override
+	public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
+		super.onBlockPlacedBy(worldIn, pos, state, placer, stack);
+		TESword te = (TESword)worldIn.getTileEntity(pos);
+		te.setModel(state.getValue(MODEL));
+	}
+	
+	@Override
+	public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ) {
+		TESword te = (TESword)world.getTileEntity(pos);
+		EnumSword modelState = te.getModel();
+
+		modelState = modelState.next();
+		
+		world.setBlockState(pos, state.withProperty(MODEL, modelState)); // this resets the TE
+		te = (TESword)world.getTileEntity(pos);
+		te.setModel(modelState);
+		return true;
+	}
+	
+	@Override
+	public IBlockState getActualState(IBlockState state, IBlockAccess worldIn, BlockPos pos) {
+		TileEntity te = worldIn.getTileEntity(pos);
+		if(te != null && te instanceof TESword) {
+			TESword sword = (TESword) te;
+			return state.withProperty(MODEL, sword.getModel());
+		}
+		return state;
+	}
+	
+	@Override
+	protected BlockStateContainer createBlockState() {
+        return new BlockStateContainer(this, new IProperty[]{ MODEL, FACING });
+    }
+	
+	@Override
+	public TileEntity createNewTileEntity(World worldIn, int meta) {
+		return new TESword();
 	}
 }
---
+++ src/main/java/me/ferdz/placeableitems/state/tool/EnumSword.java
@@ -0,0 +1,56 @@
+package me.ferdz.placeableitems.state.tool;
+
+import net.minecraft.util.IStringSerializable;
+
+/*
+ * https://i.gyazo.com/8b7171375653edef696f8eb749356454.png
+ */
+public enum EnumSword implements IStringSerializable {
+	TOP(0, "top"),
+    TOP_VERTICAL(1, "top_vertical", 0),
+    SIDE0(2, "side0"),
+    SIDE45(3, "side45"),
+    SIDE90(4, "side90"),
+    SIDE135(5, "side135"),
+    SIDE180(6, "side180"),
+    SIDE225(7, "side225"),
+    SIDE270(8, "side270"),
+    SIDE315(9, "side315"),
+    SIDE_LEAN(10, "side_lean", 2);
+    
+    private int ID;
+    private String name;
+    private int nextID;
+    
+    private EnumSword(int ID, String name) {
+    	this(ID, name, ID + 1);
+    }
+    
+    private EnumSword(int ID, String name, int next) {
+    	this.ID = ID;
+        this.name = name;
+        this.nextID = next;
+   }
+    
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public int getID() {
+        return ID;
+    }
+    
+    public EnumSword next() {
+    	for (EnumSword sword : EnumSword.values()) {
+			if(sword.getID() == nextID)
+				return sword;
+		}
+    	return null;
+    }
+    
+    @Override
+    public String toString() {
+        return getName();
+    }
+}
