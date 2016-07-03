b97fc5f4874c6859a64b2c26b976dba2e06ace15
Ferdzz <frederic.deschenes1@gmail.com>
2016-07-03 16:06:22 -0400

    Fixed missing variant exceptions

--- src/main/java/me/ferdz/placeableitems/proxy/ClientProxy.java
+++ src/main/java/me/ferdz/placeableitems/proxy/ClientProxy.java
@@ -2,6 +2,9 @@ package me.ferdz.placeableitems.proxy;
 
 import me.ferdz.placeableitems.block.tool.BlockSword;
 import me.ferdz.placeableitems.init.ModBlocks;
+import me.ferdz.placeableitems.state.EnumPreciseFacing;
+import me.ferdz.placeableitems.state.tool.EnumSword;
+import me.ferdz.placeableitems.state.tool.EnumToolMaterial;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.client.renderer.block.statemap.IStateMapper;
@@ -13,16 +16,26 @@ public class ClientProxy extends CommonProxy {
 	@Override
 	public void registerRenderers() {
 		IStateMapper map = new StateMapperBase() {
-			
 			@Override
 			protected ModelResourceLocation getModelResourceLocation(IBlockState state) {
-				return new ModelResourceLocation("placeableitems:tool/block_sword_" + state.getValue(BlockSword.MATERIAL).getName(), "facing=" + state.getValue(BlockSword.FACING).getName() + ",smodel=" + state.getValue(BlockSword.MODEL).getName());
+				EnumPreciseFacing facing = state.getValue(BlockSword.FACING);
+				EnumToolMaterial material = state.getValue(BlockSword.MATERIAL);
+				EnumSword model = state.getValue(BlockSword.MODEL);
+				
+				// this is used to remove the missing variant errors. The following models never actually happen in game
+				if(facing == EnumPreciseFacing.D135 || facing == EnumPreciseFacing.D225 || facing == EnumPreciseFacing.D315 || facing == EnumPreciseFacing.D45) {
+					if(model.getName().contains("side"))
+						facing = EnumPreciseFacing.D0;
+				}
+				
+				String location = "facing=" + facing.getName() + ",smodel=" + model.getName();
+				return new ModelResourceLocation("placeableitems:tool/block_sword_" + material, location);
 			}
 		};
-
+		
 		ModelLoader.setCustomStateMapper(ModBlocks.blockSword, map);
 	}
-
+	
 	@Override
 	public void checkUpdate() {
 	}
