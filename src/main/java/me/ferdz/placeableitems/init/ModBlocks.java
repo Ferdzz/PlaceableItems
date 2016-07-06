a4c1c62c8c08ff721ee05eb6435b2227305eba7c
Julian Meyer <julianmeyer2000@gmail.com>
2016-07-05 17:55:32 -0700

    Added snowball blocks

--- src/main/java/me/ferdz/placeableitems/init/ModBlocks.java
+++ src/main/java/me/ferdz/placeableitems/init/ModBlocks.java
@@ -74,6 +74,7 @@ public class ModBlocks {
 	public static Block blockWheat;
 	public static Block blockBeef;
 	public static Block blockPorkchop;
+	public static Block blockSnowball;
 	
 	public static void init() {
 		blockMap = new HashMap<Item, BlockPlaceableItems>();
@@ -127,6 +128,7 @@ public class ModBlocks {
 		blockWheat = new BlockPlaceableItems("block_wheat").setItem(Items.WHEAT).setBoundingBox(1, 10, 1);
 		blockBeef = new BlockBiEdible("block_beef", Items.BEEF, 3, 1.8F, Items.COOKED_BEEF, 8, 12.8F).setItem(Items.COOKED_BEEF).setItem(Items.BEEF).setBoundingBox(2, 0, 2, 14, 5, 14);
 		blockPorkchop = new BlockBiEdible("block_porkchop", Items.PORKCHOP, 3, 0.6F, Items.COOKED_PORKCHOP, 8, 12.8F).setItem(Items.PORKCHOP).setItem(Items.COOKED_PORKCHOP).setBoundingBox(2, 0, 2, 14, 5, 14);
+		blockSnowball = new BlockPlaceableItems("block_snowball").setItem(Items.SNOWBALL).setBoundingBox(3, 0, 3, 13, 10, 13);
 		
 		GameRegistry.registerTileEntity(TEStack.class, "te_stack");
 		GameRegistry.registerTileEntity(TEEdible.class, "te_edible");
