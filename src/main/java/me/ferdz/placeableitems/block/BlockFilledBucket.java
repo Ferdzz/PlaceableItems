94382aa5bfb6bc62da7706e0573f8303d389005b
Ferdz <frederic.deschenes1@gmail.com>
2016-07-08 23:01:29 -0400

    Made milk bucket drinkable

--- src/main/java/me/ferdz/placeableitems/block/BlockFilledBucket.java
+++ src/main/java/me/ferdz/placeableitems/block/BlockFilledBucket.java
@@ -1,17 +1,21 @@
 package me.ferdz.placeableitems.block;
 
 import me.ferdz.placeableitems.init.ModBlocks;
+import me.ferdz.placeableitems.tileentity.TEEdible;
+import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-public class BlockFilledBucket extends BlockPlaceableItems  {
+public class BlockFilledBucket extends BlockPlaceableItems implements ITileEntityProvider {
 
 	private Item bucketItem;
 	
@@ -27,12 +31,28 @@ public class BlockFilledBucket extends BlockPlaceableItems  {
 	@Override
 	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ) {
 		if(heldItem != null && heldItem.getItem().equals(Items.BUCKET)) {
-			if(playerIn.inventory.addItemStackToInventory(new ItemStack(bucketItem, 1))) {
-				worldIn.setBlockState(pos, ModBlocks.blockEmptyBucket.getDefaultState().withProperty(FACING, state.getValue(FACING)));
-				heldItem.stackSize--;
-				return true;
+			if(!playerIn.isCreative())
+				if(playerIn.inventory.addItemStackToInventory(new ItemStack(bucketItem, 1))) {
+					worldIn.setBlockState(pos, ModBlocks.blockEmptyBucket.getDefaultState().withProperty(FACING, state.getValue(FACING)));
+					heldItem.stackSize--;
+					return true;
+				}
+		} else if (bucketItem.equals(Items.MILK_BUCKET)) {
+			TileEntity te = worldIn.getTileEntity(pos);
+			if(te instanceof TEEdible) {
+				TEEdible te2 = (TEEdible)te;
+				if(te2.bite(0, 0, playerIn, worldIn, SoundEvents.ENTITY_GENERIC_DRINK)) {
+					playerIn.clearActivePotions();
+					worldIn.setBlockState(pos, ModBlocks.blockEmptyBucket.getDefaultState().withProperty(FACING, state.getValue(FACING)));
+				}
 			}
+			return true;
 		}
 		return false;
+	}
+
+	@Override
+	public TileEntity createNewTileEntity(World worldIn, int meta) {
+		return new TEEdible();
 	}
 }
