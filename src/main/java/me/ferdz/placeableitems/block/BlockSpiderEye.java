c3278817ea8721aa201f3c5b09da656753c7d3c8
Frederic Deschenes <frederic.deschenes1@gmail.com>
2016-08-10 01:44:38 -0400

    3.0.1.2

--- src/main/java/me/ferdz/placeableitems/block/BlockSpiderEye.java
+++ src/main/java/me/ferdz/placeableitems/block/BlockSpiderEye.java
@@ -6,13 +6,12 @@ import net.minecraft.init.Blocks;
 import net.minecraft.init.MobEffects;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-public class BlockSpiderEye extends BlockBiPositionEdible {
+public class BlockSpiderEye extends BlockEdible {
 
 	public BlockSpiderEye(String name, int foodLevel, float saturation) {
 		super(name, foodLevel, saturation);
@@ -20,10 +19,7 @@ public class BlockSpiderEye extends BlockBiPositionEdible {
 
 	@Override
 	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ) {
-		TileEntity te = worldIn.getTileEntity(pos);
-		
 		boolean b = super.onBlockActivated(worldIn, pos, state, player, hand, heldItem, side, hitX, hitY, hitZ);
-		
 		if (worldIn.getBlockState(pos).getBlock() == Blocks.AIR) {
 			player.addPotionEffect(new PotionEffect(MobEffects.POISON, 4 * 20));
 		}
