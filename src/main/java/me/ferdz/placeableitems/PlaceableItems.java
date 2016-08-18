fbd1a5bb7f71daed1293d7e0bf9b383ec6f68ae5
Ferdz <frederic.deschenes1@gmail.com>
2016-08-18 16:50:19 -0400

    3.0.2.3

--- src/main/java/me/ferdz/placeableitems/PlaceableItems.java
+++ src/main/java/me/ferdz/placeableitems/PlaceableItems.java
@@ -29,7 +29,7 @@ public class PlaceableItems {
 
 	public static final String NAME = "Placeable Items Mod";
 	public static final String MODID = "placeableitems";
-	public static final String VERSION = "3.0.2.2";
+	public static final String VERSION = "3.0.2.3";
 	
 	public static Logger logger;
 
