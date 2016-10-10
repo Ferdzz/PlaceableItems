df35d7c2211185868a69290e0c1b3826defd058e
Ferdz <frederic.deschenes1@gmail.com>
2016-10-10 18:45:00 -0400

    Fixed AI memory leak, tried making mobs react more naturally to blocks, updated plate texture

--- src/main/java/me/ferdz/placeableitems/ai/EntityAIAttractBlock.java
+++ src/main/java/me/ferdz/placeableitems/ai/EntityAIAttractBlock.java
@@ -1,6 +1,5 @@
 package me.ferdz.placeableitems.ai;
 
-import java.util.Arrays;
 import java.util.List;
 
 import net.minecraft.block.Block;
@@ -13,10 +12,11 @@ public class EntityAIAttractBlock extends EntityAIBase {
 
 	private EntityLiving entity;
 	private List<Block> blocks;
+	private BlockPos destPos;
 	
-	public EntityAIAttractBlock(EntityLiving entity, Block... blocks) {
+	public EntityAIAttractBlock(EntityLiving entity, List<Block> blocks) {
 		this.entity = entity;
-		this.blocks = Arrays.asList(blocks);
+		this.blocks = blocks;
 	}
 	
 	@Override
@@ -27,11 +27,7 @@ public class EntityAIAttractBlock extends EntityAIBase {
 					for (int k = -6; k < 7; k++) {
 						BlockPos pos = entity.getPosition().add(i, j, k);
 						if (blocks.contains(entity.getEntityWorld().getBlockState(pos).getBlock())) {
-							if (entity.getPosition().getDistance(pos.getX(), pos.getY(), pos.getZ()) > 2) {
-								pos = pos.offset(EnumFacing.getFacingFromVector(pos.getX(), pos.getY(), pos.getZ()));
-								entity.getNavigator().tryMoveToXYZ(pos.getX(), pos.getY(), pos.getZ(), 1);
-							}
-							entity.getLookHelper().setLookPosition(pos.getX() + 0.5D, pos.getY(), pos.getZ() + 0.5D, 30, 30);
+							destPos = pos.offset(EnumFacing.getFacingFromVector(pos.getX(), pos.getY(), pos.getZ()));
 							return true;
 						}
 					}
@@ -40,5 +36,26 @@ public class EntityAIAttractBlock extends EntityAIBase {
 		} catch (NullPointerException e) { /* not exactly sure why this happens sometimes */ }
 		
 		return false;
+	}
+	
+	@Override
+	public void startExecuting() {
+		entity.getLookHelper().setLookPosition(destPos.getX() + 0.5D, destPos.getY(), destPos.getZ() + 0.5D, 30, 30);
+	}
+	
+	@Override
+	public void updateTask() {
+		entity.getNavigator().setPath(entity.getNavigator().getPathToPos(destPos), 1);
+		entity.getLookHelper().setLookPosition(destPos.getX() + 0.5D, destPos.getY(), destPos.getZ() + 0.5D, 30, 30);
+	}
+	
+	@Override
+	public boolean continueExecuting() {
+		return entity.getPosition().getDistance(destPos.getX(), destPos.getY(), destPos.getZ()) > 1 || entity.getNavigator().noPath();
+	}
+
+	@Override
+	public void resetTask() {
+		destPos = null;
 	}
 }
--- src/main/java/me/ferdz/placeableitems/event/EntityJoinHandler.java
+++ src/main/java/me/ferdz/placeableitems/event/EntityJoinHandler.java
@@ -1,7 +1,11 @@
 package me.ferdz.placeableitems.event;
 
+import java.util.Arrays;
+import java.util.List;
+
 import me.ferdz.placeableitems.ai.EntityAIAttractBlock;
 import me.ferdz.placeableitems.init.ModBlocks;
+import net.minecraft.block.Block;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.passive.EntityChicken;
 import net.minecraft.entity.passive.EntityCow;
@@ -18,6 +22,18 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class EntityJoinHandler {
 
+	public static final List<Block> 
+			CHICKEN_BLOCKS = Arrays.asList(ModBlocks.blockBeetrootSeeds),
+			RABBIT_BLOCKS = Arrays.asList(ModBlocks.blockCarrot),
+			VILLAGER_BLOCKS = Arrays.asList(ModBlocks.blockEmerald),
+			WOLF_BLOCKS = Arrays.asList(ModBlocks.blockBone),
+			HORSE_BLOCKS = Arrays.asList(ModBlocks.blockApple, ModBlocks.blockGoldenApple, ModBlocks.blockCarrot, ModBlocks.blockWheat),
+			COW_BLOCKS = Arrays.asList(ModBlocks.blockWheat),
+			PIG_BLOCKS = Arrays.asList(ModBlocks.blockWheat, ModBlocks.blockCarrotOnStick, ModBlocks.blockCarrot),
+			MOOSHROOM_BLOCKS = Arrays.asList(ModBlocks.blockWheat),
+			SHEEP_BLOCKS = Arrays.asList(ModBlocks.blockWheat),
+			OCELOT_BLOCKS = Arrays.asList((Block)ModBlocks.blockFish, ModBlocks.blockSalmon, ModBlocks.blockClownfish, ModBlocks.blockPufferfish);
+	
 	@SubscribeEvent
 	public void onEntityJoinWorld(EntityJoinWorldEvent e) {
 		if(!(e.getEntity() instanceof EntityLiving))
@@ -25,27 +41,25 @@ public class EntityJoinHandler {
 		
 		EntityLiving entity = (EntityLiving) e.getEntity();
 		if(entity instanceof EntityChicken) {
-			entity.tasks.addTask(2, new EntityAIAttractBlock(entity, ModBlocks.blockBeetrootSeeds));
-		} else if (entity instanceof EntityPig || entity instanceof EntityRabbit) {
-			entity.tasks.addTask(2, new EntityAIAttractBlock(entity, ModBlocks.blockCarrot));
-		} else if (entity instanceof EntityVillager) {
-			entity.tasks.addTask(2, new EntityAIAttractBlock(entity, ModBlocks.blockEmerald));
-		} else if (entity instanceof EntityWolf) {
-			entity.tasks.addTask(2, new EntityAIAttractBlock(entity, ModBlocks.blockBone));
-		} else if (entity instanceof EntityHorse) {
-			entity.tasks.addTask(2, new EntityAIAttractBlock(entity, ModBlocks.blockApple, ModBlocks.blockGoldenApple, ModBlocks.blockCarrot, ModBlocks.blockWheat));
-		} else if (entity instanceof EntityCow) {
-			entity.tasks.addTask(2, new EntityAIAttractBlock(entity, ModBlocks.blockWheat));
-		} else if (entity instanceof EntityPig) {
-			entity.tasks.addTask(2, new EntityAIAttractBlock(entity, ModBlocks.blockWheat, ModBlocks.blockCarrotOnStick, ModBlocks.blockCarrot));
-		} else if (entity instanceof EntityMooshroom) {
-			entity.tasks.addTask(2, new EntityAIAttractBlock(entity, ModBlocks.blockWheat));
-		} else if (entity instanceof EntitySheep) {
-			entity.tasks.addTask(2, new EntityAIAttractBlock(entity, ModBlocks.blockWheat));
-		} else if (entity instanceof EntityOcelot) {
-			entity.tasks.addTask(2, new EntityAIAttractBlock(entity, ModBlocks.blockFish, ModBlocks.blockSalmon, ModBlocks.blockClownfish, ModBlocks.blockPufferfish));
+			entity.tasks.addTask(3, new EntityAIAttractBlock(entity, CHICKEN_BLOCKS));
 		} else if (entity instanceof EntityRabbit) {
-			entity.tasks.addTask(2, new EntityAIAttractBlock(entity, ModBlocks.blockCarrot));		
+			entity.tasks.addTask(3, new EntityAIAttractBlock(entity, RABBIT_BLOCKS));
+		} else if (entity instanceof EntityVillager) {
+			entity.tasks.addTask(3, new EntityAIAttractBlock(entity, VILLAGER_BLOCKS));
+		} else if (entity instanceof EntityWolf) {
+			entity.tasks.addTask(3, new EntityAIAttractBlock(entity, WOLF_BLOCKS));
+		} else if (entity instanceof EntityHorse) {
+			entity.tasks.addTask(3, new EntityAIAttractBlock(entity, HORSE_BLOCKS));
+		} else if (entity instanceof EntityCow) {
+			entity.tasks.addTask(3, new EntityAIAttractBlock(entity, COW_BLOCKS));
+		} else if (entity instanceof EntityPig) {
+			entity.tasks.addTask(3, new EntityAIAttractBlock(entity, PIG_BLOCKS));
+		} else if (entity instanceof EntityMooshroom) {
+			entity.tasks.addTask(3, new EntityAIAttractBlock(entity, MOOSHROOM_BLOCKS));
+		} else if (entity instanceof EntitySheep) {
+			entity.tasks.addTask(3, new EntityAIAttractBlock(entity, SHEEP_BLOCKS));
+		} else if (entity instanceof EntityOcelot) {
+			entity.tasks.addTask(3, new EntityAIAttractBlock(entity, OCELOT_BLOCKS));
 		}
 	}
 }
