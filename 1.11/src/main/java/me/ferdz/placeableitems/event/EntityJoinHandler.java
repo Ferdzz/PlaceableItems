e5424d669f41a92fc9caba0f7f6db29c82c3b923
Ferdz <frederic.deschenes1@gmail.com>
2017-05-20 16:08:55 -0400

    Added recipe for stands
    Disabled AI due to performance issues #21

--- 1.11/src/main/java/me/ferdz/placeableitems/event/EntityJoinHandler.java
+++ 1.11/src/main/java/me/ferdz/placeableitems/event/EntityJoinHandler.java
@@ -1,65 +1,48 @@
 package me.ferdz.placeableitems.event;
 
-import java.util.Arrays;
-import java.util.List;
-
-import me.ferdz.placeableitems.ai.EntityAIAttractBlock;
-import me.ferdz.placeableitems.init.ModBlocks;
-import net.minecraft.block.Block;
-import net.minecraft.entity.EntityLiving;
-import net.minecraft.entity.passive.EntityChicken;
-import net.minecraft.entity.passive.EntityCow;
-import net.minecraft.entity.passive.EntityHorse;
-import net.minecraft.entity.passive.EntityMooshroom;
-import net.minecraft.entity.passive.EntityOcelot;
-import net.minecraft.entity.passive.EntityPig;
-import net.minecraft.entity.passive.EntityRabbit;
-import net.minecraft.entity.passive.EntitySheep;
-import net.minecraft.entity.passive.EntityVillager;
-import net.minecraft.entity.passive.EntityWolf;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class EntityJoinHandler {
 
-	public static final List<Block> 
-			CHICKEN_BLOCKS = Arrays.asList(ModBlocks.blockBeetrootSeeds),
-			RABBIT_BLOCKS = Arrays.asList(ModBlocks.blockCarrot),
-			VILLAGER_BLOCKS = Arrays.asList(ModBlocks.blockEmerald),
-			WOLF_BLOCKS = Arrays.asList(ModBlocks.blockBone),
-			HORSE_BLOCKS = Arrays.asList(ModBlocks.blockApple, ModBlocks.blockGoldenApple, ModBlocks.blockCarrot, ModBlocks.blockWheat),
-			COW_BLOCKS = Arrays.asList(ModBlocks.blockWheat),
-			PIG_BLOCKS = Arrays.asList(ModBlocks.blockWheat, ModBlocks.blockCarrotOnStick, ModBlocks.blockCarrot),
-			MOOSHROOM_BLOCKS = Arrays.asList(ModBlocks.blockWheat),
-			SHEEP_BLOCKS = Arrays.asList(ModBlocks.blockWheat),
-			OCELOT_BLOCKS = Arrays.asList((Block)ModBlocks.blockFish, ModBlocks.blockSalmon, ModBlocks.blockClownfish, ModBlocks.blockPufferfish);
+//	public static final List<Block> 
+//			CHICKEN_BLOCKS = Arrays.asList(ModBlocks.blockBeetrootSeeds),
+//			RABBIT_BLOCKS = Arrays.asList(ModBlocks.blockCarrot),
+//			VILLAGER_BLOCKS = Arrays.asList(ModBlocks.blockEmerald),
+//			WOLF_BLOCKS = Arrays.asList(ModBlocks.blockBone),
+//			HORSE_BLOCKS = Arrays.asList(ModBlocks.blockApple, ModBlocks.blockGoldenApple, ModBlocks.blockCarrot, ModBlocks.blockWheat),
+//			COW_BLOCKS = Arrays.asList(ModBlocks.blockWheat),
+//			PIG_BLOCKS = Arrays.asList(ModBlocks.blockWheat, ModBlocks.blockCarrotOnStick, ModBlocks.blockCarrot),
+//			MOOSHROOM_BLOCKS = Arrays.asList(ModBlocks.blockWheat),
+//			SHEEP_BLOCKS = Arrays.asList(ModBlocks.blockWheat),
+//			OCELOT_BLOCKS = Arrays.asList((Block)ModBlocks.blockFish, ModBlocks.blockSalmon, ModBlocks.blockClownfish, ModBlocks.blockPufferfish);
 	
 	@SubscribeEvent
 	public void onEntityJoinWorld(EntityJoinWorldEvent e) {
-		if(!(e.getEntity() instanceof EntityLiving))
-			return;
-		
-		EntityLiving entity = (EntityLiving) e.getEntity();
-		if(entity instanceof EntityChicken) {
-			entity.tasks.addTask(3, new EntityAIAttractBlock(entity, CHICKEN_BLOCKS));
-		} else if (entity instanceof EntityRabbit) {
-			entity.tasks.addTask(3, new EntityAIAttractBlock(entity, RABBIT_BLOCKS));
-		} else if (entity instanceof EntityVillager) {
-			entity.tasks.addTask(3, new EntityAIAttractBlock(entity, VILLAGER_BLOCKS));
-		} else if (entity instanceof EntityWolf) {
-			entity.tasks.addTask(3, new EntityAIAttractBlock(entity, WOLF_BLOCKS));
-		} else if (entity instanceof EntityHorse) {
-			entity.tasks.addTask(3, new EntityAIAttractBlock(entity, HORSE_BLOCKS));
-		} else if (entity instanceof EntityCow) {
-			entity.tasks.addTask(3, new EntityAIAttractBlock(entity, COW_BLOCKS));
-		} else if (entity instanceof EntityPig) {
-			entity.tasks.addTask(3, new EntityAIAttractBlock(entity, PIG_BLOCKS));
-		} else if (entity instanceof EntityMooshroom) {
-			entity.tasks.addTask(3, new EntityAIAttractBlock(entity, MOOSHROOM_BLOCKS));
-		} else if (entity instanceof EntitySheep) {
-			entity.tasks.addTask(3, new EntityAIAttractBlock(entity, SHEEP_BLOCKS));
-		} else if (entity instanceof EntityOcelot) {
-			entity.tasks.addTask(3, new EntityAIAttractBlock(entity, OCELOT_BLOCKS));
-		}
+//		if(!(e.getEntity() instanceof EntityLiving))
+//			return;
+//		
+//		EntityLiving entity = (EntityLiving) e.getEntity();
+//		if(entity instanceof EntityChicken) {
+//			entity.tasks.addTask(3, new EntityAIAttractBlock(entity, CHICKEN_BLOCKS));
+//		} else if (entity instanceof EntityRabbit) {
+//			entity.tasks.addTask(3, new EntityAIAttractBlock(entity, RABBIT_BLOCKS));
+//		} else if (entity instanceof EntityVillager) {
+//			entity.tasks.addTask(3, new EntityAIAttractBlock(entity, VILLAGER_BLOCKS));
+//		} else if (entity instanceof EntityWolf) {
+//			entity.tasks.addTask(3, new EntityAIAttractBlock(entity, WOLF_BLOCKS));
+//		} else if (entity instanceof EntityHorse) {
+//			entity.tasks.addTask(3, new EntityAIAttractBlock(entity, HORSE_BLOCKS));
+//		} else if (entity instanceof EntityCow) {
+//			entity.tasks.addTask(3, new EntityAIAttractBlock(entity, COW_BLOCKS));
+//		} else if (entity instanceof EntityPig) {
+//			entity.tasks.addTask(3, new EntityAIAttractBlock(entity, PIG_BLOCKS));
+//		} else if (entity instanceof EntityMooshroom) {
+//			entity.tasks.addTask(3, new EntityAIAttractBlock(entity, MOOSHROOM_BLOCKS));
+//		} else if (entity instanceof EntitySheep) {
+//			entity.tasks.addTask(3, new EntityAIAttractBlock(entity, SHEEP_BLOCKS));
+//		} else if (entity instanceof EntityOcelot) {
+//			entity.tasks.addTask(3, new EntityAIAttractBlock(entity, OCELOT_BLOCKS));
+//		}
 	}
 }
--- 1.9/src/main/java/me/ferdz/placeableitems/init/ModRecipes.java
+++ 1.9/src/main/java/me/ferdz/placeableitems/init/ModRecipes.java
@@ -1,6 +1,7 @@
 package me.ferdz.placeableitems.init;
 
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -11,6 +12,23 @@ public class ModRecipes {
 				"S S",
 				" S ",
 				'S', new ItemStack(Blocks.STONE_SLAB, 1, 7)
+		});
+		
+		GameRegistry.addRecipe(new ItemStack(ModItems.itemSaddleStand), new Object[] {
+				" W ",
+				"WSW",
+				"W W",
+				'W', Blocks.PLANKS,
+				'S', Items.STICK
+		});
+		
+		GameRegistry.addRecipe(new ItemStack(ModItems.itemHorseArmorStand), new Object[] {
+				"IWI",
+				"WSW",
+				"W W",
+				'W', Blocks.PLANKS,
+				'S', Items.STICK,
+				'I', Items.IRON_INGOT
 		});
 	}
 }
