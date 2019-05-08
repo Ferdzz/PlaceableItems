967afeddb3a3313233dc1217cae13240321697ec
Ferdzz <frederic.deschenes1@gmail.com>
2019-05-07 21:05:12 -0400

    Cleanup for 1.13

--- 1.12.2/src/main/java/me/ferdz/placeableitems/block/IBlockBiPosition.java
+++
@@ -1,9 +0,0 @@
-package me.ferdz.placeableitems.block;
-
-/** 
- * This class just represents a block that handles multiple positions, no methods are required
- * @author Ferdz
- */
-public interface IBlockBiPosition {
-
-}
--- 1.9/src/main/java/me/ferdz/placeableitems/tileentity/TEFish.java
+++
@@ -1,3 +0,0 @@
-package me.ferdz.placeableitems.tileentity;
-
-public class TEFish extends TEEdibleBiPosition { }
