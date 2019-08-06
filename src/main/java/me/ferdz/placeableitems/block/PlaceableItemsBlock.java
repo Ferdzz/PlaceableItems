bcffdefaf9bd3188554d417ba156defc1029c787
Ferdzz <frederic.deschenes1@gmail.com>
2019-08-05 21:40:01 -0400

    Implement rotation of blocks

--- src/main/java/me/ferdz/placeableitems/block/PlaceableItemsBlock.java
+++ src/main/java/me/ferdz/placeableitems/block/PlaceableItemsBlock.java
@@ -1,15 +1,16 @@
 package me.ferdz.placeableitems.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
+import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.state.EnumProperty;
+import net.minecraft.state.IntegerProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
-import net.minecraft.util.BlockRenderLayer;
-import net.minecraft.util.Direction;
-import net.minecraft.util.Hand;
-import net.minecraft.util.Rotation;
+import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
@@ -18,64 +19,46 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class PlaceableItemsBlock extends Block {
+    public static final IntegerProperty ROTATION = BlockStateProperties.ROTATION_0_15;
 
     public PlaceableItemsBlock(Properties properties) {
         super(properties);
+        this.setDefaultState(this.stateContainer.getBaseState().with(ROTATION, 0));
     }
 
     @Override
     public BlockRenderLayer getRenderLayer() {
-        return BlockRenderLayer.CUTOUT;
+        return BlockRenderLayer.CUTOUT; // Enables transparency & non full block models
     }
 
+    /// Returning 1 here will disable shadow
+    @SuppressWarnings("deprecation") // This is fine to override
     @Override
-    @SuppressWarnings("deprecation")
     @OnlyIn(Dist.CLIENT)
-    public float func_220080_a(BlockState p_220080_1_, IBlockReader p_220080_2_, BlockPos p_220080_3_) {
+    public float func_220080_a(BlockState state, IBlockReader blockReader, BlockPos pos) {
         return 1.0F;
     }
 
-//    @Override
-//    public VoxelShape getShape(BlockState p_220053_1_, IBlockReader p_220053_2_, BlockPos p_220053_3_, ISelectionContext p_220053_4_) {
-//        return Block.makeCuboidShape(0, 0,0 , 1D, 1D, 1D);
-//    }
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        // Calulates the angle & maps it to the rotation state
+        return this.getDefaultState().with(ROTATION, MathHelper.floor((double) (context.getPlacementYaw() * 16.0F / 360.0F) + 0.5D) & 15);
+    }
 
-    //    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
-//        return Block.makeCuboidShape(4.0D, 0.0D, 4.0D, 12.0D, 8.0D, 12.0D);
-//    }
+    @SuppressWarnings("deprecation") // This is fine to override
+    @Override
+    public BlockState rotate(BlockState state, Rotation rot) {
+        return state.with(ROTATION, rot.rotate(state.get(ROTATION), 16));
+    }
 
-    //    @Override
-//    public boolean isVariableOpacity() {
-//        return true;
-//    }
+    @SuppressWarnings("deprecation") // This is fine to override
+    @Override
+    public BlockState mirror(BlockState state, Mirror mirrorIn) {
+        return state.with(ROTATION, mirrorIn.mirrorRotation(state.get(ROTATION), 16));
+    }
 
-    //    public static final EnumProperty<Direction.Axis> AXIS = BlockStateProperties.HORIZONTAL_AXIS;
-//
-//    @Override
-//    public BlockState rotate(BlockState state, IWorld world, BlockPos pos, Rotation direction) {
-//        switch(direction) {
-//            case COUNTERCLOCKWISE_90:
-//            case CLOCKWISE_90:
-//                switch(state.get(AXIS)) {
-//                    case X:
-//                        return state.with(AXIS, Direction.Axis.Z);
-//                    case Z:
-//                        return state.with(AXIS, Direction.Axis.X);
-//                    default:
-//                        return state;
-//                }
-//            default:
-//                return state;
-//        }
-//    }
-//
-//    @Override
-//    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
-//        builder.add(AXIS);
-//    }
-//
-//    @Override
-//    public BlockState getStateForPlacement(BlockState state, Direction facing, BlockState state2, IWorld world, BlockPos pos1, BlockPos pos2, Hand hand) {
-//        return this.getDefaultState().with(AXIS, facing.getAxis());
-//    }
+    @Override
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(ROTATION);
+    }
 }
