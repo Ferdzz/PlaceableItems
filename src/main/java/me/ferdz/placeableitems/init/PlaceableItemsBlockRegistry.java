7204fa22e4d33db772f74f78337d24c7a86f4440
Ferdzz <frederic.deschenes1@gmail.com>
2019-08-28 22:37:46 -0400

    Added bowl

--- src/main/java/me/ferdz/placeableitems/init/PlaceableItemsBlockRegistry.java
+++ src/main/java/me/ferdz/placeableitems/init/PlaceableItemsBlockRegistry.java
@@ -25,7 +25,7 @@ public class PlaceableItemsBlockRegistry {
     public static PlaceableItemsBlock BEETROOT_SOUP;
     public static PlaceableItemsBlock BLAZE_POWDER;
     public static PlaceableItemsBlock BOOK;
-
+    public static PlaceableItemsBlock BOWL;
     public static PlaceableItemsBlock WRITABLE_BOOK;
 
     @SubscribeEvent
@@ -35,21 +35,25 @@ public class PlaceableItemsBlockRegistry {
                 .build()
                 .setShape(VoxelShapesUtil.create(16, 4, 16))
                 .register("bone_block", Items.BONE);
+        // TODO: Make edible
         APPLE = new PlaceableItemsBlockBuilder()
                 .addComponent(new BiPositionBlockComponent())
                 .build()
                 .setShape(VoxelShapesUtil.create(5, 9, 5))
                 .register("apple_block", Items.APPLE);
+        // TODO: Make edible
         BEEF = new PlaceableItemsBlockBuilder()
                 .addComponent(new BiPositionBlockComponent())
                 .build()
                 .setShape(VoxelShapesUtil.create(6, 3, 6))
                 .register("beef_block", Items.BEEF);
+        // TODO: Make edible
         BEEF_COOKED =  new PlaceableItemsBlockBuilder()
                 .addComponent(new BiPositionBlockComponent())
                 .build()
                 .setShape(VoxelShapesUtil.create(6, 3, 6))
                 .register("beef_cooked_block", Items.COOKED_BEEF);
+        // TODO: Make edible
         BEETROOT = new PlaceableItemsBlockBuilder()
                 .build()
                 .setShape(VoxelShapesUtil.create(5, 6, 5))
@@ -58,6 +62,7 @@ public class PlaceableItemsBlockRegistry {
                 .build()
                 .setShape(VoxelShapesUtil.create(5, 2, 5))
                 .register("beetroot_seeds_block", Items.BEETROOT_SEEDS);
+        // TODO: Make edible
         BEETROOT_SOUP =  new PlaceableItemsBlockBuilder()
                 .build()
                 .setShape(VoxelShapesUtil.create(7, 4, 7))
@@ -71,6 +76,13 @@ public class PlaceableItemsBlockRegistry {
                 .build()
                 .setShape(VoxelShapesUtil.create(7, 6, 7))
                 .register("book_block", Items.BOOK);
+        BOWL = new PlaceableItemsBlockBuilder()
+                .build()
+                .setShape(VoxelShapesUtil.create(8, 4, 8))
+                .register("bowl_block", Items.BOWL);
+        
+
+
         // TODO: Allow for writing to the book when placed?
         // FIXME: The book is opened when placed, that interaction needs to be stopped somehow
         WRITABLE_BOOK = new PlaceableItemsBlockBuilder()
