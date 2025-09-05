83ac1c5d3297ff1db7e00c96b381ba0d15828218
Ferdzz <frederic.deschenes1@gmail.com>
2025-09-05 15:08:07 -0400

    Setup block builder & handle hitbox shape

--- src/main/java/me/ferdz/placeableitems/init/PlaceableItemsBlockEntityTypeRegistry.java
+++ src/main/java/me/ferdz/placeableitems/init/PlaceableItemsBlockEntityTypeRegistry.java
@@ -3,6 +3,7 @@ package me.ferdz.placeableitems.init;
 import me.ferdz.placeableitems.PlaceableItems;
 import me.ferdz.placeableitems.block.blockentity.StackHolderBlockEntity;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
@@ -15,16 +16,18 @@ public class PlaceableItemsBlockEntityTypeRegistry {
     public static final Supplier<BlockEntityType<StackHolderBlockEntity>> STACK_HOLDER = BLOCK_ENTITY_TYPES.register(
             "stack_holder_block_entity",
             // The block entity type, created using a builder.
-            () -> BlockEntityType.Builder.of(
-                            // The supplier to use for constructing the block entity instances.
-                            StackHolderBlockEntity::new,
-                            // A vararg of blocks that can have this block entity.
-                            // This assumes the existence of the referenced blocks as DeferredBlock<Block>s.
-                    // TODO: Plug all types of blocks
-                            PlaceableItemsBlockRegistry.COAL_BLOCK.get() //, MyBlocks.MY_BLOCK_2.get()
-                    )
-                    // Build using null; vanilla does some datafixer shenanigans with the parameter that we don't need.
-                    .build(null)
+            () -> {
+                return BlockEntityType.Builder.of(
+                                // The supplier to use for constructing the block entity instances.
+                                StackHolderBlockEntity::new,
+                                // A vararg of blocks that can have this block entity.
+                                // This assumes the existence of the referenced blocks as DeferredBlock<Block>s.
+                                // TODO: Plug all types of blocks
+                                PlaceableItemsMap.instance().values().toArray(new Block[0])
+                        )
+                        // Build using null; vanilla does some datafixer shenanigans with the parameter that we don't need.
+                        .build(null);
+            }
     );
 
 }
--- src/main/java/me/ferdz/placeableitems/init/PlaceableItemsBlockRegistry.java
+++ src/main/java/me/ferdz/placeableitems/init/PlaceableItemsBlockRegistry.java
@@ -2,13 +2,9 @@ package me.ferdz.placeableitems.init;
 
 import me.ferdz.placeableitems.PlaceableItems;
 import me.ferdz.placeableitems.block.PlaceableItemsBlock;
-import net.minecraft.client.resources.model.Material;
+import me.ferdz.placeableitems.block.PlaceableItemsBlockBuilder;
+import me.ferdz.placeableitems.utils.VoxelShapesUtil;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
-import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
@@ -16,26 +12,7 @@ public final class PlaceableItemsBlockRegistry {
     // Create a Deferred Register to hold Blocks which will all be registered under the "placeableitems" namespace
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(PlaceableItems.MODID);
 
-    // Creates a new Block with the id "placeableitems:example_block", combining the namespace and path
-    public static final DeferredBlock<PlaceableItemsBlock> COAL_BLOCK =
-            // TODO: Move to builder
-            BLOCKS.register("coal_block", () ->
-                    {
-                        PlaceableItemsBlock block = new PlaceableItemsBlock(
-                                BlockBehaviour.Properties
-                                        .of() // starts with a blank slate
-                                        // TODO: Check for properties in survival
-//                                    .strength(0.5f)
-
-                                        .noOcclusion()
-//                                    .isViewBlocking((state, worlds, pos) -> false)
-//                                    .isSuffocating((state, world, pos) -> false)
-//                                    .isRedstoneConductor((state, world, pos) -> false)
-//                                    .sound(SoundType.WOOD)
-                        );
-                        PlaceableItemsMap.instance().put(Items.COAL, block);
-                        return block;
-                    }
-            );
-
+    public static final DeferredBlock<PlaceableItemsBlock> COAL_BLOCK = PlaceableItemsBlockBuilder.of()
+            .setShape(VoxelShapesUtil.create(10, 6, 10))
+            .register("coal",Items.COAL);
 }
